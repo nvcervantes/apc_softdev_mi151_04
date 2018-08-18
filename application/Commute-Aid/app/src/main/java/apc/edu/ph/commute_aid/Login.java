a036b6189651eeeb4363308de0180a558ba292e0
Shierene Cervantes <shierenecervantes23@gmail.com>
2018-08-18 21:37:55 +0800

    Added login and register

---
+++ application/Commute-Aid/app/src/main/java/apc/edu/ph/commute_aid/Login.java
@@ -0,0 +1,21 @@
+package apc.edu.ph.commute_aid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_login);
+    }
+    public void login(View v) {
+        startActivity(new Intent(Login.this,MainActivity.class));
+    }
+    public void register(View v) {
+        startActivity(new Intent(Login.this, Register.class));
+    }
+}
